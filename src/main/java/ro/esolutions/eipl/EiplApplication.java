f4431a16ebbf698cd9dea18d4c4579f66d91c05b
dan <dan.visan@esolutions.ro>
2018-08-14 15:04:22 +0300

    Initial commit

---
+++ src/main/java/ro/esolutions/eipl/EiplApplication.java
@@ -0,0 +1,12 @@
+package ro.esolutions.eipl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EiplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EiplApplication.class, args);
+	}
+}
