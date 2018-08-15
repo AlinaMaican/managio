d87a4213aa9b115f5e83733c466171cafb077380
popescustefanradu <popescustefanradu@gmail.com>
2018-08-15 15:14:03 +0300

    Initial frontend setup for user functionality

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { DetailsComponent } from './user/details/details.component';
+import { AddeditComponent } from './user/addedit/addedit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    DetailsComponent,
+    AddeditComponent
   ],
   imports: [
     BrowserModule
