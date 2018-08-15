560fec0766240c562278e49ee65682b2a1bd16d2
Robert Ivan <robert.ivan@esolutions.ro>
2018-08-15 15:42:07 +0300

    Refactor user module

--- webapp/src/app/users/form/form.component.ts
+++ webapp/src/app/users/form/form.component.ts
@@ -2,10 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-addedit',
-  templateUrl: './addedit.component.html',
-  styleUrls: ['./addedit.component.css']
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
 })
-export class AddeditComponent implements OnInit {
+export class FormComponent implements OnInit {
 
   constructor() { }
 
