feefc89facca7afdf2a5c425d4c944e49916c13d
popescustefanradu <popescustefanradu@gmail.com>
2018-08-20 18:31:55 +0300

    Add login functionality

--- src/main/java/ro/esolutions/eipl/controllers/LoginController.java
+++ src/main/java/ro/esolutions/eipl/controllers/LoginController.java
@@ -1,16 +1,32 @@
 package ro.esolutions.eipl.controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ro.esolutions.eipl.models.LoginFormModel;
+
+import java.security.Principal;
 
 @Controller
-@RequestMapping("/login")
+@RequestMapping
 public class LoginController {
 
-    @GetMapping
-    public String getLogin(){
+    @GetMapping("/login")
+    public String getLogin(Model model) {
+        model.addAttribute("loginForm", new LoginFormModel());
         return "login";
+    }
+
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, path = "/welcome")
+    public String welcome(Model model) {
+        // TODO why arg injection for Principal doesn't work
+        Authentication principal = SecurityContextHolder.getContext().getAuthentication();
+        model.addAttribute("username", principal.getName());
+        return "welcome";
     }
 
 }
---
+++ src/main/java/ro/esolutions/eipl/mappers/UserDetailsImplMapper.java
@@ -0,0 +1,24 @@
+package ro.esolutions.eipl.mappers;
+
+import ro.esolutions.eipl.configurations.UserDetailsImpl;
+import ro.esolutions.eipl.entities.User;
+
+import java.util.Collections;
+
+public class UserDetailsImplMapper {
+    private UserDetailsImplMapper() {
+    }
+
+    public static UserDetailsImpl fromUser(User user) {
+        return UserDetailsImpl.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .accountNonExpired(true)
+                .accountNonLocked(true)
+                .credentialsNonExpired(true)
+                .grantedAuthorities(Collections.singletonList(user.getUserRole()))
+                .enabled(user.getIsActive())
+                .build();
+    }
+
+}
