1d91abc280bc3a7bf6d36409a8c3d0d83a8828d1
Andreea Popovici <adapopovici33@gmail.com>
2018-08-20 10:33:05 +0300

    - modified exception

--- src/main/java/ro/esolutions/eipl/exceptions/UserAlreadyExistsException.java
+++ src/main/java/ro/esolutions/eipl/exceptions/UserAlreadyExistsException.java
@@ -2,9 +2,9 @@ package ro.esolutions.eipl.exceptions;
 
 public class UserAlreadyExistsException extends RuntimeException {
 
-    private static final String MESSAGE = "User with id %s already exists!";
+    private static final String MESSAGE = "User with username %s already exists!";
 
-    public UserAlreadyExistsException(final String  username) {
+    public UserAlreadyExistsException(final String username) {
 
         super(String.format(MESSAGE, username));
     }
