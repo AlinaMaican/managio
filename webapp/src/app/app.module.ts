f4270123ee7503ba96da571a24915726a28ad5ff
victoria <victoria.modoran@esolutions.ro>
2018-08-22 16:10:46 +0300

    refactor

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { DetailsComponent } from './users/details/details.component';
 import { FormComponent } from './users/form/form.component';
 import { UserService } from './users/user.service';
 import {HttpClientModule} from '@angular/common/http';
+import {UserProfileComponent} from "./user-profile/user-profile.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     UserListComponent,
     DetailsComponent,
-    FormComponent
+    FormComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
---
+++ webapp/src/app/app.router.module.ts
@@ -0,0 +1,20 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from "@angular/router";
+import {UserListComponent} from "./users/user-list.component";
+// import {AddUserComponent} from "./users/add-user/add-user.component";
+import {UserProfileComponent} from "./user-profile/user-profile.component";
+
+const routes: Routes = [
+  {path: '', component: UserListComponent},
+  // {path: 'add-user', component: AddUserComponent},
+  {path: 'resetPassword', component: UserProfileComponent}
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule],
+  declarations: []
+})
+export class AppRooterModule { }
