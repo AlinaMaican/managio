a392b85eb744d69776bb1cb6970b70eabe821852
Crihan Alexandru <alexandru.crihan@esolutions.ro>
2018-08-23 14:54:10 +0300

    revert

--- src/main/java/ro/esolutions/eipl/configurations/WebConfig.java
+++ src/main/java/ro/esolutions/eipl/configurations/WebConfig.java
@@ -2,15 +2,14 @@ package ro.esolutions.eipl.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("forward:index.html");
-    }
+@EnableWebMvc
+public class WebConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
