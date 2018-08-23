65993feb4f39c1306fb5997b5640db67f4cbe024
popescustefanradu <popescustefanradu@gmail.com>
2018-08-23 16:29:51 +0300

    Fix .gitignore and WebConfig to enable one-build configuration

--- src/main/java/ro/esolutions/eipl/configurations/WebConfig.java
+++ src/main/java/ro/esolutions/eipl/configurations/WebConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
