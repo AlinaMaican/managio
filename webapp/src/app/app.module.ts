d8692623d82e32642f5e24f9c6d372a2929b7c5f
Mircea Malaescu <mircea.malaescu@gmailcom>
2018-08-24 17:46:54 +0300

    add nav bar component

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -7,13 +7,17 @@ import { DetailsComponent } from './users/details/details.component';
 import { FormComponent } from './users/form/form.component';
 import { UserService } from './users/user.service';
 import {HttpClientModule} from '@angular/common/http';
+import {AppDropdownDirective} from "./shared/app-dropdown.directive";
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserListComponent,
     DetailsComponent,
-    FormComponent
+    FormComponent,
+    AppDropdownDirective,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
