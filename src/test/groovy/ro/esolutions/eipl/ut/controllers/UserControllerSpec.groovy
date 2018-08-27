fa6b28661dfee09aefb48f7ded387d721aefd14e
Vlad Daineanu <daineanu.vlad@gmail.com>
2018-08-27 15:56:44 +0300

    add edit service test

--- src/test/groovy/ro/esolutions/eipl/ut/controllers/UserControllerSpec.groovy
+++ src/test/groovy/ro/esolutions/eipl/ut/controllers/UserControllerSpec.groovy
@@ -3,13 +3,11 @@ package ro.esolutions.eipl.ut.controllers
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.BindingResult
 import ro.esolutions.eipl.controllers.UserController
-import ro.esolutions.eipl.generator.UserModelGenerator
 import ro.esolutions.eipl.services.UserService
 import spock.lang.Specification
 import spock.lang.Unroll
 
 import static ro.esolutions.eipl.generator.UserModelGenerator.aUserModel
-
 
 class UserControllerSpec extends Specification {
 
