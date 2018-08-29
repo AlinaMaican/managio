0d773fd48c5006dbd50e274b0624c58d02a71ae4
popescustefanradu <popescustefanradu@gmail.com>
2018-08-29 12:56:23 +0300

    Clean up code for the LoginController integration test

--- src/test/groovy/ro/esolutions/eipl/controllers/LoginControllerITSpec.groovy
+++ src/test/groovy/ro/esolutions/eipl/controllers/LoginControllerITSpec.groovy
@@ -1,58 +1,52 @@
 package ro.esolutions.eipl.controllers
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.jdbc.Sql
 import org.springframework.test.context.jdbc.SqlGroup
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.ResultMatcher
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers
-import org.springframework.test.web.servlet.setup.MockMvcBuilders
-import org.springframework.web.context.WebApplicationContext;
-import ro.esolutions.eipl.EiplApplication;
+import org.springframework.transaction.annotation.Transactional
+import ro.esolutions.eipl.EiplApplication
 import ro.esolutions.eipl.configurations.EmbeddedPostgreSQLConfiguration
 import ro.esolutions.eipl.configurations.SecurityConfig
 import ro.esolutions.eipl.models.UserRole
 import spock.lang.Specification
 import spock.lang.Unroll
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 import static ro.esolutions.eipl.controllers.LoginController.LOGIN_PATH_FULL
 import static ro.esolutions.eipl.controllers.LoginController.WELCOME_PATH_FULL
-import static ro.esolutions.eipl.generators.UserDetailsGenerator.*;
+import static ro.esolutions.eipl.generators.UserDetailsGenerator.aUserDetails;
 
 @ActiveProfiles('integration')
 @ContextConfiguration(classes = [EiplApplication, EmbeddedPostgreSQLConfiguration, SecurityConfig])
 @SpringBootTest
+@AutoConfigureMockMvc
 @SqlGroup([
         @Sql(value = ['/sql/addUsers.sql'], executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
-        @Sql(value = ['/sql/deleteAllUsers.sql'], executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 ])
+@Transactional
 class LoginControllerITSpec extends Specification {
 //        to print results remove unroll and add:
 //        result.andDo(MockMvcResultHandlers.print())
 
     private static final String DOMAIN_MATCHER = "http://*"
-    private static final ResultMatcher LOGIN_ERROR_REDIRECT_MATCHER = redirectedUrl("${LOGIN_PATH_FULL}?error")
+    private static final String LOGIN_ERROR_PATH = "${LOGIN_PATH_FULL}?error"
+    private static final ResultMatcher LOGIN_ERROR_REDIRECT_MATCHER = redirectedUrl(LOGIN_ERROR_PATH)
 
     @Autowired
-    WebApplicationContext wac
     MockMvc mockMvc
 
-    def setup() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(wac).apply(springSecurity()).build()
-    }
 
     @Unroll
-//    @WithMockUser
     def 'test login page with anonymous'() {
         when:
         def result = mockMvc.perform(get(getLoginUrl).with(csrf()).with(anonymous()))
@@ -67,7 +61,7 @@ class LoginControllerITSpec extends Specification {
         '/asd'          | status().is3xxRedirection() | redirectedUrlPattern(DOMAIN_MATCHER + LOGIN_PATH_FULL)
     }
 
-    @Unroll
+    @Unroll("Authentication with #username/#password should yield #resultStatusName and redirect to #redirectUrl")
     def 'test authentication'() {
         when:
         def result = mockMvc.perform(post(LOGIN_PATH_FULL)
@@ -75,25 +69,26 @@ class LoginControllerITSpec extends Specification {
                 .param('username', username)
                 .param('password', password))
         then:
-        if (expectedRedirectUrl) {
-            result.andExpect(expectedRedirectUrl)
+        if (expectedRedirectUrlMatcher) {
+            result.andExpect(expectedRedirectUrlMatcher)
         }
         where:
-        username         | password        | expectedResultStatus        | expectedRedirectUrl
-        'activeUser'     | 'asd'           | status().is3xxRedirection() | redirectedUrl(WELCOME_PATH_FULL)
-        'inactiveUser'   | 'asd'           | status().is3xxRedirection() | LOGIN_ERROR_REDIRECT_MATCHER
-        'inexistentUser' | 'asd'           | status().is3xxRedirection() | LOGIN_ERROR_REDIRECT_MATCHER
-        'activeUser'     | 'wrongPassword' | status().is3xxRedirection() | LOGIN_ERROR_REDIRECT_MATCHER
+        resultStatusName << ['3xxRedirection'] * 4
+        username | password | expectedResultStatus | expectedRedirectUrlMatcher | redirectUrl
+        'activeUser' | 'asd' | status().is3xxRedirection() | redirectedUrl(WELCOME_PATH_FULL) | WELCOME_PATH_FULL
+        'inactiveUser' | 'asd' | status().is3xxRedirection() | LOGIN_ERROR_REDIRECT_MATCHER | LOGIN_ERROR_PATH
+        'inexistentUser' | 'asd' | status().is3xxRedirection() | LOGIN_ERROR_REDIRECT_MATCHER | LOGIN_ERROR_PATH
+        'activeUser' | 'wrongPass' | status().is3xxRedirection() | LOGIN_ERROR_REDIRECT_MATCHER | LOGIN_ERROR_PATH
     }
 
 //  page lacks role-based security
     def 'test welcome page'() {
+        given:
+        def userDetails = aUserDetails([authorities: [UserRole.USER]])
+        def expectedResultStatus = status().isOk()
         when:
         def result = mockMvc.perform(get(WELCOME_PATH_FULL).with(user(userDetails)))
         then:
         result.andExpect(expectedResultStatus)
-        where:
-        userDetails                                  | expectedResultStatus
-        aUserDetails([authorities: [UserRole.USER]]) | status().isOk()
     }
 }
--- src/test/resources/sql/deleteAllUsers.sql
+++
@@ -1,2 +0,0 @@
-delete from users
-where id IN (-1, -2);
