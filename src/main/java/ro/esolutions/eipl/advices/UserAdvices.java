cdadc527fa60c7e49bb692313b440957954327bb
popescustefanradu <popescustefanradu@gmail.com>
2018-08-30 10:47:47 +0300

    Remove errors that are always checked using model validation

--- src/main/java/ro/esolutions/eipl/advices/UserAdvices.java
+++ src/main/java/ro/esolutions/eipl/advices/UserAdvices.java
@@ -1,11 +1,6 @@
 package ro.esolutions.eipl.advices;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ro.esolutions.eipl.exceptions.UserAlreadyExistsException;
-import ro.esolutions.eipl.exceptions.UserEmailAlreadyExists;
-import ro.esolutions.eipl.models.FieldErrorModel;
 
 @RestControllerAdvice
 public class UserAdvices {
