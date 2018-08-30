681361787135944855eb0178403363496b6ccede
victoria <victoria.modoran@esolutions.ro>
2018-08-30 14:37:47 +0300

    reformat code

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,21 +1,21 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { UserListComponent } from './users/user-list.component';
-import { DetailsComponent } from './users/details/details.component';
-import { FormComponent } from './users/form/form.component';
-import { UserService } from './users/user.service';
-import { HttpClientModule} from '@angular/common/http';
-import { HeaderComponent } from './header/header.component';
-import { FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { AppRouterModule} from "./app.router.module";
-import { EditComponent} from './users/edit/edit.component';
-import { AddUserComponent} from "./users/add-user/add-user.component";
-import { UserProfileComponent } from "./users/user-profile/user-profile.component";
-import { EmployeeService} from "./employees/employee.service";
-import { EquipmentsComponent} from "./equipments/equipments.component";
-import { EquipmentService} from "./equipments/equipment.service";
+import {AppComponent} from './app.component';
+import {UserListComponent} from './users/user-list.component';
+import {DetailsComponent} from './users/details/details.component';
+import {FormComponent} from './users/form/form.component';
+import {UserService} from './users/user.service';
+import {HttpClientModule} from '@angular/common/http';
+import {HeaderComponent} from './header/header.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {AppRouterModule} from "./app.router.module";
+import {EditComponent} from './users/edit/edit.component';
+import {AddUserComponent} from "./users/add-user/add-user.component";
+import {UserProfileComponent} from "./users/user-profile/user-profile.component";
+import {EmployeeService} from "./employees/employee.service";
+import {EquipmentsComponent} from "./equipments/equipments.component";
+import {EquipmentService} from "./equipments/equipment.service";
 
 @NgModule({
   declarations: [
