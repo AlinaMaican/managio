b78b79c02493715f8ffbfd1795f6984572228e74
Denis Amegica <amegica.denis@gmail.com>
2018-08-30 14:17:26 +0300

    add employee controller test

---
+++ src/test/groovy/ro/esolutions/eipl/ut/controllers/EmployeeControllerSpec.groovy
@@ -0,0 +1,31 @@
+package ro.esolutions.eipl.ut.controllers
+
+import org.springframework.http.ResponseEntity
+import ro.esolutions.eipl.controllers.EmployeeController
+import ro.esolutions.eipl.services.EmployeeService
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import static ro.esolutions.eipl.generators.EmployeeModelGenerator.aEmployeeModel
+
+class EmployeeControllerSpec extends Specification {
+
+    def employeeService = Mock(EmployeeService)
+    def employeeController = new EmployeeController(employeeService)
+
+    @Unroll
+    def "getAllEmployees"() {
+        given:
+
+        when:
+        def result = employeeController.getAllEmployees()
+
+        then:
+        result == ResponseEntity.ok([aEmployeeModel()])
+
+        and:
+        1 * employeeService.getAllEmployees() >> [aEmployeeModel()]
+        0 * _
+
+    }
+}
