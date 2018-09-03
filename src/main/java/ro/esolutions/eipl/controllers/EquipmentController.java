afc364631b978f2f62fae58ed558125db87f8b23
Andreea Popovici <adapopovici33@gmail.com>
2018-09-03 17:49:17 +0300

    - resolved conflicts with master

--- src/main/java/ro/esolutions/eipl/controllers/EquipmentController.java
+++ src/main/java/ro/esolutions/eipl/controllers/EquipmentController.java
@@ -1,6 +1,5 @@
 package ro.esolutions.eipl.controllers;
 
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
