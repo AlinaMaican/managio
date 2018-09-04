99f1f1c8c1bb35ecfa11ea53c110e486f9306f13
Andreea Popovici <adapopovici33@gmail.com>
2018-09-04 15:46:54 +0300

    - resolved confilcts

--- src/main/java/ro/esolutions/eipl/exceptions/EquipmentUploadFileNotValid.java
+++ src/main/java/ro/esolutions/eipl/exceptions/EquipmentUploadFileNotValid.java
@@ -5,7 +5,6 @@ public class EquipmentUploadFileNotValid extends RuntimeException {
     private static final String MESSAGE = "File to be uploaded is not valid!";
 
     public EquipmentUploadFileNotValid() {
-
         super(String.format(MESSAGE));
     }
 }
